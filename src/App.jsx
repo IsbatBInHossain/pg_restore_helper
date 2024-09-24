f30282605991ee81b81ade4ed75236929e5c413e
Isbat-Bin-Hossain <isbat31416@gmail.com>
2024-09-24 09:39:16 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className=''>
+      <h1>Pg Restore Commanad maker</h1>
+      <p>Create custom pg_restore command easily</p>
+    </div>
+  )
+}
+
+export default App
